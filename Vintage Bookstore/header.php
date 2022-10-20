8646bdbf6c505644f4103931ec6a6d2d10520d5b
archaion <80986857+archaion@users.noreply.github.com>
2022-10-20 15:33:16 -0400

    Update 10/20/22

--- Vintage Bookstore/header.php
+++ Vintage Bookstore/header.php
@@ -12,7 +12,7 @@
    <div id='overlay'>
       <div id='banner'></div>
       <div id='gradient'></div>
-      <div id='owner'><span>Hunter:</span></div>
+      <div id='owner'><span>Werwolf:</span></div>
       <div id='splat'></div>
       <div id='streak'></div>
       <header class='title'>
@@ -21,19 +21,18 @@
          <h1><a class='print' href='<?php bloginfo('url'); ?>'><em>Moonlight</em>
             </a></h1>
          <div id='ampersand'><em>&amp;</em></div>
-         <h2><a class='print' href='<?php bloginfo('url'); ?>'><em>Blood</em>
+         <h2><a class='print' href='<?php bloginfo('url'); ?>'><em>Thorns</em>
             </a></h2>
       </header>
       <!--<div id='line'></div>-->
       <aside class='subtitle'>
          <!-- links to main pages -->
-         <p id='address1'><em>Studio</em></p>
-         <p id='address2'><em>Crossroad</em></p>
-         <p id='address3'><em>2022</em></p>
+         <span id='copy1'><em>Pennyblood Comics</em></span>
+         <span id='copy2'><em>2022</em></span>
       </aside>
       <div id='divider'>
          <div id='menu'>
-            <a id='books' href='<?php echo bloginfo('url') . '/books'; ?>'>BOOKS</a>|<a id='artwork' href='<?php echo bloginfo('url') . '/artwork'; ?>'>ARTWORK</a>|<a id='contact' href='<?php echo bloginfo('url') . '/contact'; ?>'>CONTACT</a>
+            <a id='books' href='<?php echo bloginfo('url') . '/read'; ?>'>READ</a>|<a id='artwork' href='<?php echo bloginfo('url') . '/studio'; ?>'>STUDIO</a>|<a id='contact' href='<?php echo bloginfo('url') . '/contact'; ?>'>CONTACT</a>
          </div>
       </div>
       <?php get_sidebar(); ?>
